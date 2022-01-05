33da49a328c57620439b9f68970dee81982dfeda
hareshnayak <sketchharry01@gmail.com>
2022-01-05 13:04:28 +0530

    Database created

--- app/src/main/java/com/hareshnayak/shoplist/db/ShoppingDatabase.kt
+++ app/src/main/java/com/hareshnayak/shoplist/db/ShoppingDatabase.kt
@@ -1,6 +1,8 @@
 package com.hareshnayak.shoplist.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 
@@ -14,6 +16,20 @@ abstract class ShoppingDatabase : RoomDatabase() {     // ShoppingDatabase inher
 
     // Only a single instance of the Database is required, hence, this is a Singleton
     companion object{
+        @Volatile       // this instance will be visible to other threads too
+        private var instance: ShoppingDatabase?= null
+        private val LOCK  = Any()
 
+        // This function is called when the instance of Database is created
+        operator fun invoke(context: Context) = instance ?:
+        // if instance is null then called synchronized so that the instance is in one thread only
+        synchronized(LOCK){
+            instance ?:
+            // if the instance is still null then createDatabase
+            createDatabase(context).also{ instance = it }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext, ShoppingDatabase::class.java, "ShoppingDB.db").build()
     }
 }
